cd9f903552c460b567b414036180d0c45804bd59
MariamGad <mariamgad142@gmail.com>
2023-05-28 03:38:02 +0300

    private ec2

--- ec2.tf
+++ ec2.tf
@@ -17,6 +17,30 @@ resource "aws_instance" "bastion_host" {
     vpc_security_group_ids =[aws_security_group.allow_ssh.id]
     key_name = "aws_keys_pairs"
     tags = {
-        Name = "${var.NAME}_${each.key}_bastion"
+    Name = "${var.NAME}_${each.key}_bastion"
+    }
+    depends_on =[tls_private_key.private_key]
+    provisioner "file" {
+        source      = "tf-key-pair.pem"
+        destination = "/home/ec2-user/tf-key-pair.pem"
+        
+        connection {
+        type        = "ssh"
+        user        = "ec2-user"
+        private_key = tls_private_key.private_key.private_key_pem
+        host        = "${self.public_ip}"
+        }
+    }
+
+}
+resource "aws_instance" "application" {
+    ami = data.aws_ami.amazon-linux-2023-ami.id
+    instance_type = "t2.micro"
+    for_each = aws_subnet.private_subnet
+    subnet_id = each.value.id
+    vpc_security_group_ids =[aws_security_group.private_ssh.id]
+    key_name = "aws_keys_pairs"
+    tags = {
+        Name = "${var.NAME}_${each.key}_app"
     }
 }
--- security group.tf
+++ security group.tf
@@ -2,10 +2,21 @@ resource "aws_security_group" "allow_ssh"{
     name        = "${var.NAME}_allow_ssh"
     vpc_id      = aws_vpc.main.id
     ingress {
-        description      = "SSH from VPC"
         from_port        = 22
         to_port          = 22
         protocol         = "tcp"
         cidr_blocks=["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "private_ssh"{
+    name        = "${var.NAME}_private_ssh"
+    vpc_id      = aws_vpc.main.id
+    ingress {
+        description      = "SSH from VPC"
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks=[aws_vpc.main.cidr_block]
     }
 }
