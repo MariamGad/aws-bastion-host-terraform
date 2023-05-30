e742f134abc1c4d9b152171afaa522fdedabd7b5
MariamGad <mariamgad142@gmail.com>
2023-05-30 12:54:18 +0300

    ssh from bastion

--- security group.tf
+++ security group.tf
@@ -7,6 +7,14 @@ resource "aws_security_group" "allow_ssh"{
         protocol         = "tcp"
         cidr_blocks=["0.0.0.0/0"]
     }
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    
 }
 
 resource "aws_security_group" "private_ssh"{
